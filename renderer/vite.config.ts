a5bd4c78b79bed61b2654aa9986241cb34699b80
LittleSheep <littlesheep.code@hotmail.com>
2023-07-14 16:26:55 +0800

    :bug: Fix i18n slot error in production

--- renderer/vite.config.ts
+++ renderer/vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue"
 import AutoImport from "unplugin-auto-import/vite"
 import Components from "unplugin-vue-components/vite"
 import { NaiveUiResolver } from "unplugin-vue-components/resolvers"
-import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,7 +22,7 @@ export default defineConfig({
     Components({
       resolvers: [NaiveUiResolver()],
     }),
-    VueI18nPlugin({}),
+    VueI18nPlugin({ runtimeOnly: false }),
   ],
   resolve: {
     alias: {
