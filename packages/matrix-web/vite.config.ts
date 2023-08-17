4ef5e2d78c45bae5cb31154ed32ceea60df7baca
LittleSheep <littlesheep.code@hotmail.com>
2023-08-18 00:19:09 +0800

    :bug: Bug fixes

--- packages/matrix-web/vite.config.ts
+++ packages/matrix-web/vite.config.ts
@@ -1,39 +1,43 @@
-import { fileURLToPath, URL } from "node:url"
+import {fileURLToPath, URL} from "node:url"
 
-import { defineConfig } from "vite"
+import {defineConfig} from "vite"
 import Vue from "@vitejs/plugin-vue"
 import AutoImport from "unplugin-auto-import/vite"
 import Components from "unplugin-vue-components/vite"
-import { NaiveUiResolver } from "unplugin-vue-components/resolvers"
+import {NaiveUiResolver} from "unplugin-vue-components/resolvers"
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/srv/subapps/matrix",
-  plugins: [
-    Vue(),
-    AutoImport({
-      imports: [
-        "vue",
-        {
-          "naive-ui": ["useDialog", "useMessage", "useNotification", "useLoadingBar"],
+    base: "/srv/subapps/matrix",
+    plugins: [
+        Vue(),
+        AutoImport({
+            imports: [
+                "vue",
+                {
+                    "naive-ui": ["useDialog", "useMessage", "useNotification", "useLoadingBar"],
+                },
+            ],
+        }),
+        Components({
+            resolvers: [NaiveUiResolver()],
+        }),
+        VueI18nPlugin({runtimeOnly: false}),
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
-      ],
-    }),
-    Components({
-      resolvers: [NaiveUiResolver()],
-    }),
-    VueI18nPlugin({ runtimeOnly: false }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-  },
-  server: {
-    port: 5176,
-    proxy: {
-      "/api": "http://127.0.0.1:9446",
+    server: {
+        port: 5176,
+        proxy: {
+            "/srv/subapps/matrix/api": {
+                target: "http://127.0.0.1:9446",
+                changeOrigin: true,
+                rewrite: (path) => path.replace("/srv/subapps/matrix", "")
+            }
+        },
     },
-  },
 })
